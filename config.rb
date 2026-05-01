c58658aa98e9ea667df6ee0b24499b678bcf9e8e
beeps <kimberly.grey@digital.cabinet-office.gov.uk>
2026-05-01 08:57:23 +0100

    Add `updated` front matter and last updated dates. (#194)

--- config.rb
+++ config.rb
@@ -2,6 +2,15 @@ require 'govuk_tech_docs'
 
 GovukTechDocs.configure(self)
 
+# use custom.erb layout file
+set :layout, 'custom'
+
 # use relative paths for links and sources
 activate :relative_assets
 set :relative_links, true
+
+helpers do
+  def format_short_date(date)
+    Date.strptime(date, "%Y-%m").strftime("%B %Y")
+  end
+end
